347b88ad0e8318c74cffdeea09ccfa5282d646ec
EdR-maker <eduardorocha564@gmail.com>
2024-06-22 16:36:33 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- cartorio.e.cpp
+++ cartorio.e.cpp
@@ -127,10 +127,9 @@ int main()
 	printf("Escolha a opção desejada no menu:\n\n");
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
-	printf ("\t3 - Deletar nomes\n\n");
+	printf ("\t3 - Deletar nomes\n");
+	printf("\t4 - Sair do sistema\n\n");
 	printf("opção: ");
-	
-	
 	scanf("%d", &opcao);
 	
 	system("cls");
@@ -147,6 +146,11 @@ int main()
 		
 		case 3: 
 		deleta();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!\n");
+		return 0;
 		break;
 		
 		default:
